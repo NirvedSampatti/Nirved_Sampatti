17cdcf5019c1f22284bbb87f9965a13e3605a2d8
Mehul Chawda <mehulhasmail@gmail.com>
2025-07-26 20:38:24 +0530

    Add /nirvedsampatti route

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/nirvedsampatti/")
 def home():
-    return "Nirved Sampatti is live!"
+    return "Nirved Sampatti is live at /nirvedsampatti!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
